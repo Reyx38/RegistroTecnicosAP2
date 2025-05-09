d76e18dcf6ffb08577a8cdd1c12d736b57e0dacf
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-05-09 00:58:54 -0400

    Creando archivo kotlin que contiene los campos de la clase de tecnico llamado tecnicoEntity.kt

---
+++ app/src/main/java/edu/ucne/registrotecnicosap2/Data/Entities/tecnicoEntity.kt
@@ -0,0 +1,11 @@
+package edu.ucne.registrotecnicosap2.Data.Entities
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Tecnicos")
+data class tecnicoEntity (
+    @PrimaryKey
+    val tecnicoId: Int? = null,
+    val nombre: String = "",
+    val sueldoHora: Float = 0.0F
+)
