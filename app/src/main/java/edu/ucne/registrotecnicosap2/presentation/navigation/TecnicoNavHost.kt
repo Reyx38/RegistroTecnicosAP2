681dc2902b94fd384d7e26596bed801c31de75ff
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-05-16 22:15:29 -0400

    Modificando el host para agregar las proriedades

--- app/src/main/java/edu/ucne/registrotecnicosap2/presentation/navigation/TecnicoNavHost.kt
+++ app/src/main/java/edu/ucne/registrotecnicosap2/presentation/navigation/TecnicoNavHost.kt
@@ -6,7 +6,11 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
+import edu.ucne.registrotecnicosap2.Data.Entities.PrioridadEntity
 import edu.ucne.registrotecnicosap2.Data.Entities.TecnicoEntity
+import edu.ucne.registrotecnicosap2.presentation.Prioridades.PrioridadScreen
+import edu.ucne.registrotecnicosap2.presentation.Prioridades.PrioridadViewModel
+import edu.ucne.registrotecnicosap2.presentation.Prioridades.PrioridadesListScreen
 import edu.ucne.registrotecnicosap2.presentation.tecnicos.TecnicoListScreen
 import edu.ucne.registrotecnicosap2.presentation.tecnicos.TecnicoScreen
 import edu.ucne.registrotecnicosap2.presentation.tecnicos.TecnicosViewModel
@@ -15,13 +19,16 @@ import edu.ucne.registrotecnicosap2.presentation.tecnicos.TecnicosViewModel
 fun TecnicoNavHost(
     navHostController: NavHostController,
     tecnicoList: List<TecnicoEntity>,
+    prioridadesList: List<PrioridadEntity?>,
     viewModel: TecnicosViewModel,
+    PrioridadviewModel: PrioridadViewModel,
     navcontrol: NavController
 ) {
     NavHost(
         navController = navHostController,
         startDestination = Screen.TecnicoList
     ) {
+
         composable<Screen.TecnicoList> {
             TecnicoListScreen(
                 tecnicoList = tecnicoList,
@@ -30,6 +37,12 @@ fun TecnicoNavHost(
                 },
                 onDelete = { tecnico ->
                     viewModel.deleteTecnico(tecnico)
+                },
+                onNavigateToPrioridades = {
+                    navHostController.navigate(Screen.PrioridadList)
+                },
+                onNavigateToTickets ={
+                    navHostController.navigate(Screen.TicketList)
                 }
             )
         }
@@ -38,5 +51,27 @@ fun TecnicoNavHost(
             val tecnicoId = backStack.toRoute<Screen.Tecnico>().tecnicoId
             TecnicoScreen(tecnicoId, navcontrol, viewModel)
         }
+
+        composable<Screen.PrioridadList> {
+            PrioridadesListScreen(
+                prioridadesList = prioridadesList,
+                onEdit = { prioridadId ->
+                    navHostController.navigate(Screen.Prioridad(prioridadId))
+
+                },
+                onDelete = { prioridad  ->
+                    PrioridadviewModel.deletePrioridad(prioridad = prioridad)
+                },
+                navController = navcontrol
+            )
+        }
+
+        composable<Screen.Prioridad> { backStack ->
+            val prioridadId = backStack.toRoute<Screen.Prioridad>().prioridad
+            PrioridadScreen(prioridadId, navcontrol, PrioridadviewModel)
+        }
+
+        composable <Screen.TicketList>{ }
+        composable <Screen.Ticket> { }
     }
 }
