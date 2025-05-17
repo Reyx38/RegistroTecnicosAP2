d6c3fd4ef2ca9195998f7e039abb372acc7d6020
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-05-16 22:16:55 -0400

    Modificacion del Screen para agregar los datos de las siguientes pantallas

--- app/src/main/java/edu/ucne/registrotecnicosap2/presentation/navigation/Screen.kt
+++ app/src/main/java/edu/ucne/registrotecnicosap2/presentation/navigation/Screen.kt
@@ -7,4 +7,12 @@ sealed class Screen{
     data object TecnicoList : Screen()
     @Serializable
     data class Tecnico (val tecnicoId: Int?) : Screen()
+    @Serializable
+    data object PrioridadList: Screen()
+    @Serializable
+    data class Prioridad(val prioridad: Int?): Screen()
+    @Serializable
+    data object TicketList : Screen()
+    @Serializable
+    data class  Ticket (val ticketId: Int?) : Screen()
 }
