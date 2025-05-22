6362f1c96d078e9d1a90529303a7fa0b46fc708f
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-05-22 12:54:45 -0400

    Implemtentacion de libreria

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     // Room + KSP
     implementation("androidx.room:room-runtime:2.7.1")
     implementation("androidx.room:room-ktx:2.7.1")
+    implementation(libs.androidx.room.compiler)
     ksp("androidx.room:room-compiler:2.7.1")
 
     // Hilt
