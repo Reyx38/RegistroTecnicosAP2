bc20d20f1a4ad1ed4662eb3f5aff8e7a516a83fb
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-05-23 01:09:27 -0400

    Agregando el Uistate al view model y usnado los eventos creados

--- app/src/main/java/edu/ucne/registrotecnicosap2/presentation/tecnicos/TecnicosViewModel.kt
+++ app/src/main/java/edu/ucne/registrotecnicosap2/presentation/tecnicos/TecnicosViewModel.kt
@@ -4,28 +4,119 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import edu.ucne.registrotecnicosap2.Data.Entities.TecnicoEntity
 import edu.ucne.registrotecnicosap2.Data.repository.TecnicoRepository
-
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class TecnicosViewModel(
+class TecnicosViewModel @Inject constructor(
     private val tecnicoRepository: TecnicoRepository
-): ViewModel() {
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(TecnicoUiState())
+    val uiState = _uiState.asStateFlow()
 
-    fun saveTecnico(tecnico: TecnicoEntity){
-        viewModelScope.launch {
-            tecnicoRepository.save(tecnico)
+    init {
+        getTecnico()
+    }
+
+    fun onEvent(event: TecnicoEvent) {
+        when (event) {
+            is TecnicoEvent.TecnicoChange -> onTecnicoChange(event.tecnicoId)
+            is TecnicoEvent.NombreChange -> onNombreChange(event.nombre)
+            is TecnicoEvent.SueldoHoraChange -> onSueldoHoraChange(event.sueldoHora)
+            TecnicoEvent.Delete -> TODO()
+            TecnicoEvent.New -> TODO()
+            TecnicoEvent.Save -> TODO()
+        }
+
+    }
+
+    private fun onSueldoHoraChange(sueldoHora: Float?) {
+        _uiState.update {
+            it.copy(
+                sueldoHora = sueldoHora
+            )
         }
     }
-    fun getTecnicoById(id: Int, onResult: (TecnicoEntity?) -> Unit) {
-        viewModelScope.launch {
-            val tecnico = tecnicoRepository.find(id)
-            onResult(tecnico)
+
+    private fun onNombreChange(nombre: String) {
+        _uiState.update {
+            it.copy(
+                nombre = nombre
+            )
         }
     }
-    fun deleteTecnico(tecnico: TecnicoEntity){
-        viewModelScope.launch {
-            tecnicoRepository.delete(tecnico)
+
+    private fun onTecnicoChange(tecnicoId: Int) {
+        _uiState.update {
+            it.copy(
+                tecnicoId = tecnicoId
+            )
         }
     }
+
+    private fun getTecnico() {
+        viewModelScope.launch {
+            tecnicoRepository.getAll().collect { tecnicos ->
+                _uiState.update {
+                    it.copy(
+                        tecnicos = tecnicos
+                    )
+                }
+            }
+        }
+    }
+
+    fun saveTecnico(tecnico: TecnicoEntity) {
+        viewModelScope.launch {
+            if (_uiState.value.nombre.isNullOrBlank()) {
+                _uiState.update {
+                    it.copy(
+                        nombreErrorMessage = "El nombre esta vacio"
+                    )
+                }
+
+            } else {
+                if (_uiState.value.sueldoHora!! < 0.0f) {
+                    _uiState.update {
+                        it.copy(
+                            sueldoErrorMessage = "El sueldo no puede ser menor a cero"
+                        )
+                    }
+                } else {
+                    tecnicoRepository.save(_uiState.value.toEntity())
+                }
+            }
+        }
+    }
+
+    fun getTecnicoById(tecnicoId: Int) {
+        viewModelScope.launch {
+            if (tecnicoId > 0) {
+                val tecnico = tecnicoRepository.find(tecnicoId)
+                _uiState.update {
+                    it.copy(
+                        tecnicoId = tecnico?.tecnicoId,
+                        nombre = tecnico?.nombre,
+                        sueldoHora = tecnico?.sueldoHora
+                    )
+                }
+            }
+
+        }
+    }
+
+    fun deleteTecnico() {
+        viewModelScope.launch {
+            tecnicoRepository.delete(_uiState.value.toEntity())
+        }
+    }
+
+    fun TecnicoUiState.toEntity() = TecnicoEntity(
+        tecnicoId = tecnicoId,
+        nombre = nombre ?: "",
+        sueldoHora = sueldoHora ?: 0.0f
+    )
 
 }
