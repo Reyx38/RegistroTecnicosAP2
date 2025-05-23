bc016ada0b8f401339c035a309777437d2bef2d7
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-05-23 01:09:46 -0400

    Limpiando el codigo del main

--- app/src/main/java/edu/ucne/registrotecnicosap2/MainActivity.kt
+++ app/src/main/java/edu/ucne/registrotecnicosap2/MainActivity.kt
@@ -4,77 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.*
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.compose.LocalLifecycleOwner
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.rememberNavController
-import androidx.room.Room
 import dagger.hilt.android.AndroidEntryPoint
-import edu.ucne.registrotecnicosap2.Data.Database.TecnicoDb
-import edu.ucne.registrotecnicosap2.Data.repository.PrioridadRepository
-import edu.ucne.registrotecnicosap2.Data.repository.TecnicoRepository
-import edu.ucne.registrotecnicosap2.Data.repository.TicketRepository
-import edu.ucne.registrotecnicosap2.presentation.Prioridades.PrioridadViewModel
 import edu.ucne.registrotecnicosap2.presentation.navigation.TecnicoNavHost
-import edu.ucne.registrotecnicosap2.presentation.tecnicos.TecnicosViewModel
-import edu.ucne.registrotecnicosap2.presentation.ticket.TicketViewModel
 import edu.ucne.registrotecnicosap2.ui.theme.RegistroTecnicosAP2Theme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private lateinit var tecnicoDb: TecnicoDb
-    private lateinit var tecnicosRepository: TecnicoRepository
-    private lateinit var tecnicosViewModel: TecnicosViewModel
-    private lateinit var prioridadRepository: PrioridadRepository
-    private lateinit var prioridadViewModel: PrioridadViewModel
-    private lateinit var ticketRepository: TicketRepository
-    private lateinit var ticketViewModel: TicketViewModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        tecnicosRepository = TecnicoRepository(tecnicoDb.tecnicoDao())
-        tecnicosViewModel = TecnicosViewModel(tecnicosRepository)
-        prioridadRepository = PrioridadRepository(tecnicoDb.prioridadDao())
-        prioridadViewModel = PrioridadViewModel(prioridadRepository)
-        ticketRepository = TicketRepository(tecnicoDb.ticketDao())
-        ticketViewModel = TicketViewModel(ticketRepository)
-
         setContent {
-            val lifecycleOwner = LocalLifecycleOwner.current
-            val tecnicoList by tecnicoDb.tecnicoDao().getAll()
-                .collectAsStateWithLifecycle(
-                    initialValue = emptyList(),
-                    lifecycleOwner = lifecycleOwner,
-                    minActiveState = Lifecycle.State.STARTED
-                )
-            val prioridadList by tecnicoDb.prioridadDao().getAll()
-                .collectAsStateWithLifecycle(
-                    initialValue = emptyList(),
-                    lifecycleOwner = lifecycleOwner,
-                    minActiveState = Lifecycle.State.STARTED
-                )
-            val ticketList by tecnicoDb.ticketDao().getAll()
-                .collectAsStateWithLifecycle(
-                    initialValue = emptyList(),
-                    lifecycleOwner = lifecycleOwner,
-                    minActiveState = Lifecycle.State.STARTED
-                )
-
-
             RegistroTecnicosAP2Theme {
                 val nav = rememberNavController()
-                TecnicoNavHost(
-                    nav,
-                    tecnicoList,
-                    prioridadList,
-                    ticketList,
-                    tecnicosViewModel,
-                    prioridadViewModel,
-                    ticketViewModel,
-                    nav
-                )
+                TecnicoNavHost(nav)
             }
         }
     }
