4df2c7b660cb79c88b59a8ae67941c546efc87a2
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-05-24 14:54:34 -0400

    Actulizando imports

--- app/src/main/java/edu/ucne/registrotecnicosap2/presentation/navigation/TecnicoNavHost.kt
+++ app/src/main/java/edu/ucne/registrotecnicosap2/presentation/navigation/TecnicoNavHost.kt
@@ -1,25 +1,16 @@
 package edu.ucne.registrotecnicosap2.presentation.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
-import edu.ucne.registrotecnicosap2.Data.Entities.PrioridadEntity
-import edu.ucne.registrotecnicosap2.Data.Entities.TecnicoEntity
-import edu.ucne.registrotecnicosap2.Data.Entities.TicketEntity
-import edu.ucne.registrotecnicosap2.presentation.Prioridades.PrioridadScreen
-import edu.ucne.registrotecnicosap2.presentation.Prioridades.PrioridadViewModel
-import edu.ucne.registrotecnicosap2.presentation.Prioridades.PrioridadesListScreen
+import edu.ucne.registrotecnicosap2.presentation.prioridades.PrioridadScreen
+import edu.ucne.registrotecnicosap2.presentation.prioridades.PrioridadesListScreen
 import edu.ucne.registrotecnicosap2.presentation.tecnicos.TecnicoListScreen
 import edu.ucne.registrotecnicosap2.presentation.tecnicos.TecnicoScreen
-import edu.ucne.registrotecnicosap2.presentation.tecnicos.TecnicosViewModel
 import edu.ucne.registrotecnicosap2.presentation.ticket.TicketListScreen
 import edu.ucne.registrotecnicosap2.presentation.ticket.TicketScreen
-import edu.ucne.registrotecnicosap2.presentation.ticket.TicketViewModel
 
 @Composable
 fun TecnicoNavHost(
@@ -72,7 +63,7 @@ fun TecnicoNavHost(
         composable<Screen.Prioridad> { backStack ->
             val prioridadId = backStack.toRoute<Screen.Prioridad>().prioridad
             PrioridadScreen(
-                prioridadId,
+               prioridadId =  prioridadId,
                 goBack = {
                     navHostController.navigateUp()
                 }
@@ -82,32 +73,22 @@ fun TecnicoNavHost(
 
         composable<Screen.TicketList> {
             TicketListScreen(
-                ticketList = ticketList,
-                tecnicoList = tecnicoList,
-                prioridadList = prioridadesList,
                 onEdit = { ticketId ->
                     navHostController.navigate(Screen.Ticket(ticketId))
                 },
-                onDelete = { ticket ->
-                    ticketViewModel.deleteTicket(ticket)
-                },
-                onNavigationToTecnico = {
-                    navHostController.navigate(Screen.TecnicoList)
-                },
-                onNavigationToPrioridad = {
-                    navHostController.navigate(Screen.PrioridadList)
-                },
-                navcontrol
+                createTicket = {
+                    navHostController.navigate(Screen.Ticket(0))
+                }
+
             )
         }
         composable<Screen.Ticket> { backStack ->
             val ticketId = backStack.toRoute<Screen.Ticket>().ticketId
             TicketScreen(
                 ticketId = ticketId,
-                tecnicos = tecnicoList,
-                prioridades = prioridadesList,
-                navcontrol,
-                ticketViewModel
+                goBack = {
+                    navHostController.navigateUp()
+                }
             )
         }
     }
