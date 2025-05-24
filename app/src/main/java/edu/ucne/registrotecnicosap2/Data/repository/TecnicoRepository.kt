fc1293f635473b97bdd0b381d1d62a79d12fe185
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-05-24 14:55:08 -0400

    Agregando la notacion

--- app/src/main/java/edu/ucne/registrotecnicosap2/Data/repository/TecnicoRepository.kt
+++ app/src/main/java/edu/ucne/registrotecnicosap2/Data/repository/TecnicoRepository.kt
@@ -3,8 +3,11 @@ package edu.ucne.registrotecnicosap2.Data.repository
 import edu.ucne.registrotecnicosap2.Data.Dao.TecnicoDao
 import edu.ucne.registrotecnicosap2.Data.Entities.TecnicoEntity
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class TecnicoRepository (
+@Singleton
+class TecnicoRepository @Inject constructor(
     private val dao: TecnicoDao
 ) {
     suspend fun save(tecnico: TecnicoEntity) = dao.save(tecnico)
